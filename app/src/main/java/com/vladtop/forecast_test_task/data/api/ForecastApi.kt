b6dc3d74476db649c834125901c3be9cc272f77d
Vladislav Topolnyk <byntar33@gmail.com>
2023-02-14 11:24:34 +0200

    update API KEY

--- app/src/main/java/com/vladtop/forecast_test_task/data/api/ForecastApi.kt
+++ app/src/main/java/com/vladtop/forecast_test_task/data/api/ForecastApi.kt
@@ -8,14 +8,14 @@ import retrofit2.http.Query
 interface ForecastApi {
     companion object {
         const val BASE_URL = "https://api.weatherapi.com/v1/"
-        private const val API_KEY = "099141aeefb24d4a9c7214818222312"
+        private const val API_KEY = "c7393549829240c4bbb92158231402"
     }
 
     @GET("forecast.json?key=$API_KEY")
-    suspend fun getForecast(
-        @Query("q") query: String,
-        @Query("days") days: Int = 4,
-        @Query("aqi") aqi: String = "no",
-        @Query("alerts") alerts: String = "no"
-    ): Response<ForecastDTO>
+                suspend fun getForecast(
+            @Query("q") query: String,
+            @Query("days") days: Int = 3,
+            @Query("aqi") aqi: String = "no",
+            @Query("alerts") alerts: String = "no"
+        ): Response<ForecastDTO>
 }
